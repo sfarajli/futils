df7092717ed497d1615c1d39a6fa00f2fffe205a
Suleyman Farajli <suleyman@farajli.net>
2024-11-22 18:22:13 +0400

    errprintf function added

---
+++ src/errprintf.c
@@ -0,0 +1,33 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "util.h"
+
+char * progname = NULL;
+
+void
+errprintf(const int errval, char *msg, ...)
+{
+	va_list ap;
+
+	if (progname && msg[0] == ':') {
+		fprintf(stderr, "%s: ", progname);
+		msg++;
+	}
+
+	va_start(ap, msg);
+	vfprintf(stderr, msg, ap);
+	va_end(ap);
+
+	if (msg[0] && msg[strlen(msg)-1] == ':') {
+		fputc(' ', stderr);
+		perror(NULL);
+	} else {
+		putchar('\n');
+	}
+
+	if (errval != 0)
+		exit(errval);
+}
--- src/util.h
+++ src/util.h
@@ -3,8 +3,12 @@
 
 #include <sys/types.h>
 #include <ftw.h>
+#include <stddef.h>
+
+extern char * progname;
 
 int parsemode(char * str, mode_t * mode);
+void errprintf(const int errval, char *msg, ...);
 
 void walk(char * path,
                int (* func) (const char * path, const struct stat *sb, int tflag, struct FTW * ftwbuf),
