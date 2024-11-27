f875bd9456b16c7286157695462d3a168f3a28ee
Suleyman Farajli <suleyman@farajli.net>
2024-11-27 19:00:18 +0400

    link added

---
+++ src/link.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+#include "util.h"
+
+int
+main(int argc, char **argv)
+{
+	if (argc != 3)
+		errprintf(1, ":Invalid usage\nSee the man page for help");
+
+	if (link(argv[1], argv[2]))
+		errprintf(1, ":failed to link '%s' -> '%s':", argv[2], argv[1]);
+
+	return 0;
+}
