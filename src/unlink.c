796ab5720092e1ca9030289e2fd00b2514fa80c5
Suleyman Farajli <suleyman@farajli.net>
2024-11-27 19:02:28 +0400

    unlink added

---
+++ src/unlink.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+#include "util.h"
+
+int
+main(int argc, char ** argv)
+{
+	int retval = 0;
+	for (int i = 1; i < argc; i++) {
+		if (unlink(argv[i])) {
+			errprintf(0, ":failed to unlink file '%s':", argv[i]);
+			retval = 1;
+		}
+	}
+
+	return retval;
+}
