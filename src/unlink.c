1971eb7a90460deeea8e78f9570ba7dbe40c7dd0
Suleyman Farajli <suleyman@farajli.net>
2024-11-28 14:55:32 +0400

    unlink: progname set

--- src/unlink.c
+++ src/unlink.c
@@ -6,6 +6,8 @@ int
 main(int argc, char ** argv)
 {
 	int retval = 0;
+	progname = argv[0];
+
 	for (int i = 1; i < argc; i++) {
 		if (unlink(argv[i])) {
 			errprintf(0, ":failed to unlink file '%s':", argv[i]);
