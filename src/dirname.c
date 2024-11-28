764cb5965ac5ddfda15fa89897a231ed95ec5d76
Suleyman Farajli <suleyman@farajli.net>
2024-11-28 09:23:16 +0400

    dirname added

---
+++ src/dirname.c
@@ -0,0 +1,15 @@
+#include <libgen.h>
+#include <stdio.h>
+
+#include "util.h"
+
+int
+main(int argc, char **argv)
+{
+	if (argc != 2)
+		errprintf(1, ":operand is missing\nSee the man page for help");
+
+	printf("%s\n", dirname(argv[1]));
+	/* FIXME: Flushing might be necessary */
+	return 0;
+}
